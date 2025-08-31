f29a8f9b9d4a49f6346365cee6826cd0ec80f33b
26Naitik <naitikgupta803@gmail.com>
2025-08-31 15:32:47 +0530

    Expression.java

---
+++ Oprators_&_Expressions/Expression.java
@@ -0,0 +1,10 @@
+public class Expression {
+    public static void main(String[] args) {
+        int u=2;
+        int v=3;
+        int a=12;
+        int s=4;
+        float a1 =(float) ((v*v)-(u*u))/(2*a*s);
+        System.out.println(a1);
+    }
+}
