d24543a312b0a976d67b9ca25469c9d0f4eac278
26Naitik <naitikgupta803@gmail.com>
2025-09-01 19:26:11 +0530

    Escape.java

---
+++ Strings/Escape.java
@@ -0,0 +1,8 @@
+//write a java program to format the following letter using escpae sequence charecters .
+
+public class Escape {
+    public static void main(String[] args) {
+        String letter = "Dear Naitik,\n\tThis Java course is nice.\nThanks!";
+        System.out.println(letter);
+    }
+}
