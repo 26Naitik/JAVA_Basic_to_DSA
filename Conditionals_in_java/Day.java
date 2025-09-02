95350a66db92e0db11ac65718d208fd2decba034
26Naitik <naitikgupta803@gmail.com>
2025-09-02 22:24:37 +0530

    Day.java

---
+++ Conditionals_in_java/Day.java
@@ -0,0 +1,37 @@
+//write a java program to print the day by taking input from the user as number(1-7).
+import java.util.Scanner;
+public class Day {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter the number: ");
+        int day = sc.nextInt();
+
+        switch (day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+
+            default:
+            System.out.println("Not a valid date");
+            sc.close();
+        }
+    }
+}
