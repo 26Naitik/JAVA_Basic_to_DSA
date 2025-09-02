c0fd9f922ebd698728e404af67c1bba3cc88389b
26Naitik <naitikgupta803@gmail.com>
2025-09-02 22:25:48 +0530

    Output.java

---
+++ Conditionals_in_java/Output.java
@@ -0,0 +1,14 @@
+//what will be the output of this program ?
+
+public class Output{
+    public static void main(String[] args) {
+        int a=10;
+        if (a==11){
+            System.out.println("I am 11");
+        } else {
+            System.out.println("I am not 11");
+        }
+    }
+}
+
+//bas == me error ayega
