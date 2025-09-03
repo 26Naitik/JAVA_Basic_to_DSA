ea68d2f5e18fef485efcc3992bfe5cb268d7cbf6
26Naitik <naitikgupta803@gmail.com>
2025-09-03 21:14:30 +0530

    For_Two.java

---
+++ Loop_Control_Instructions/For_Two.java
@@ -0,0 +1,12 @@
+public class For_Two {
+    public static void main(String[] args) {
+        int n=2;
+        for(int i=1;i<=n;i++){
+            System.out.println(2);
+        }
+    }
+}
+
+//Theory question 
+//what can be done using one type of loop can also be done ysing other type of loop (true/false)?
+//=> true
