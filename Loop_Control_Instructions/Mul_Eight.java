5455c202962c54231a91167e404eca49196bc896
26Naitik <naitikgupta803@gmail.com>
2025-09-03 21:15:03 +0530

    Mul_Eight.java

---
+++ Loop_Control_Instructions/Mul_Eight.java
@@ -0,0 +1,16 @@
+public class Mul_Eight {
+    public static void main(String[] args) {
+        int n=10;
+        int number =8;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum +=number*i;
+        }
+        System.out.println(sum);
+    }
+}
+
+
+//Theory question
+//A do while loop is executed ___________?
+//=>At least once.
