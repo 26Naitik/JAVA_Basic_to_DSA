9237fd88ecc8c1a37dc3277765a9b9cc122e94d6
26Naitik <naitikgupta803@gmail.com>
2025-09-03 21:14:02 +0530

    Five.java

---
+++ Loop_Control_Instructions/Five.java
@@ -0,0 +1,10 @@
+//write a java program to print 5 using while loop
+public class Five{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=5){
+            System.out.println(5);
+            i++;
+        }
+    }
+}
