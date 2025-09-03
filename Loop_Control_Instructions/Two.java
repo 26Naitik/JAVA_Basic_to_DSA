38384808f0db9ffd3418608550829be76c8ac0a9
26Naitik <naitikgupta803@gmail.com>
2025-09-03 21:16:55 +0530

    Two.java

---
+++ Loop_Control_Instructions/Two.java
@@ -0,0 +1,11 @@
+//write a java program to print 2 using while loop
+
+public class Two{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=2){
+            System.out.println(2);
+            i++;
+        }
+    }
+}
