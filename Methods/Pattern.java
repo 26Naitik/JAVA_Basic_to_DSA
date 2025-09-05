5882f6bac4ab77f3c35804bbae44259e1cc81ad4
26Naitik <naitikgupta803@gmail.com>
2025-09-05 23:53:37 +0530

    Pattern.java

---
+++ Methods/Pattern.java
@@ -0,0 +1,14 @@
+//write a java program to print pattern 1 using methods
+public class Pattern {
+    static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+        pattern1(4);
+    }
+}
