bd9910ef2ac3e46620034994c02767879910e032
26Naitik <naitikgupta803@gmail.com>
2025-09-05 23:52:48 +0530

    four.java

---
+++ Methods/Four.java
@@ -0,0 +1,13 @@
+//write a java program to repeat four using methods
+public class Four {
+    static void repFour(int n){
+         int i=1;
+        while(i<=4){
+            System.out.println(4);
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        repFour(4);
+    }
+}
