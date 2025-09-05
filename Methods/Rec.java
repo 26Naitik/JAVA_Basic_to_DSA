df374cac455183f43e9c5d91ae004287af9209a9
26Naitik <naitikgupta803@gmail.com>
2025-09-05 23:54:21 +0530

    Rec.java

---
+++ Methods/Rec.java
@@ -0,0 +1,13 @@
+//write a java progra to print sum of n number using methods
+public class Rec {
+    static int sumRec(int n){
+        if(n==1){
+            return 1;
+        }
+        return n + sumRec(n-1);
+    }
+    public static void main(String[] args) {
+        int c = sumRec(10);
+        System.out.println(c);
+    }
+}
