2c1c2d3e5d4ae7665161f1d13f56b5295ca01181
26Naitik <naitikgupta803@gmail.com>
2025-09-05 23:54:41 +0530

    Two.java

---
+++ Methods/Two.java
@@ -0,0 +1,13 @@
+//write a java program to repeat two using methods
+public class Two {
+    static void repTwo(int n){
+         int i=1;
+        while(i<=2){
+            System.out.println(2);
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        repTwo(2);
+    }
+}
