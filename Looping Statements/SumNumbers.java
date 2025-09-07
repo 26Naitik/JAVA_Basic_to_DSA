62ebeead1348fda6e9cb624e4134f0538fdead69
26Naitik <naitikgupta803@gmail.com>
2025-09-07 22:46:39 +0530

    SumNumbers.java

---
+++ Looping Statements/SumNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumNumbers {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of n:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
