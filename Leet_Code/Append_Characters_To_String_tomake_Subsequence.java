3224bfe5f5e7baad476b3ad4c2ae127920908a79
26Naitik <naitikgupta803@gmail.com>
2025-09-09 22:27:22 +0530

    Problem_1229.java

---
+++ Leet_Code/Append_Characters_To_String_tomake_Subsequence.java
@@ -0,0 +1,22 @@
+public class Append_Characters_To_String_tomake_Subsequence{
+    public static void main(String[] args) {
+        String s= "coaching";
+    String t = "coading";
+    int i=0;
+    int j=0;
+    int n=s.length();
+    int m=t.length();
+
+    while (i<n && j<m){
+        if (s.charAt(i)== t.charAt(j)){
+            i++;
+            j++;
+        }
+        else {
+            i++;
+        }
+    }
+    int result = m-j;
+    System.out.println(result);
+    }
+}
